f0fd39343e836d7902402e1fe8103a2847369f29
Sricharran <sricharran2801@gmail.com>
2026-03-20 04:10:42 -0400

    task3

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -5,38 +5,40 @@ const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
 
 const app = express();
-app.use("*",cors());
 const port = 3060;
 
-// Connect to MongoDB
-connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
-})
-.catch((e) => console.error('Failed to connect to DB', e));
-
+// Middleware
+app.use(cors());
 app.use(express.json());
 
-// Route files
-// Gift API Task 1
-const giftRoutes = require('./routes/giftRoutes');
+// Connect to MongoDB
+connectToDatabase()
+    .then(() => {
+        pinoLogger.info('Connected to DB');
+    })
+    .catch((e) => {
+        console.error('Failed to connect to DB', e);
+    });
 
-// Search API Task 1
+// Route files
+const giftRoutes = require('./routes/giftRoutes');
 const searchRoutes = require('./routes/searchRoutes');
 
+// Logger middleware
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
-
 app.use(pinoHttp({ logger }));
 
 // Use Routes
-// Gift API Task 2
 app.use('/api/gifts', giftRoutes);
-
-// Search API Task 2
 app.use('/api/search', searchRoutes);
+
+// Root route
+app.get("/", (req, res) => {
+    res.send("Inside the server");
+});
 
 // Global Error Handler
 app.use((err, req, res, next) => {
@@ -44,10 +46,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error');
 });
 
-app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
-
+// Start server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
